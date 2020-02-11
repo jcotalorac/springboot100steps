92af4b152c3d72946ef662c44314439d8a9ebb9e
juan.otalora <juancarlos.otalora@wnco.com>
2020-02-11 15:51:59 -0500

    Explicit mapping of get request, instead of default (all methods)

--- spring-boot-first-web-application/src/main/java/com/in28minutes/springboot/web/springbootfirstwebapplication/controller/LoginController.java
+++ spring-boot-first-web-application/src/main/java/com/in28minutes/springboot/web/springbootfirstwebapplication/controller/LoginController.java
@@ -3,11 +3,12 @@ package com.in28minutes.springboot.web.springbootfirstwebapplication.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
 
-	@RequestMapping("/login")
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String loginMessage(ModelMap model) {
 		//model.put("name", name);
 		//System.out.println("name is: " + name);

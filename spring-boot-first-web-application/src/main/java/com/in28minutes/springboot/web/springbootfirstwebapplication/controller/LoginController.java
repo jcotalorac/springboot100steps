54d012559c9bb6650a71c616472196e5bc0a6ba2
juan.otalora <juancarlos.otalora@wnco.com>
2020-02-11 14:33:23 -0500

    Use of requestParam to get query params for get request

--- spring-boot-first-web-application/src/main/java/com/in28minutes/springboot/web/springbootfirstwebapplication/controller/LoginController.java
+++ spring-boot-first-web-application/src/main/java/com/in28minutes/springboot/web/springbootfirstwebapplication/controller/LoginController.java
@@ -2,13 +2,15 @@ package com.in28minutes.springboot.web.springbootfirstwebapplication.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
 
 	@RequestMapping("/login")
-	public String loginMessage() {
+	public String loginMessage(@RequestParam(name = "name") String name) {
+		System.out.println("name is: " + name);
 		return "login";
 	}
 }

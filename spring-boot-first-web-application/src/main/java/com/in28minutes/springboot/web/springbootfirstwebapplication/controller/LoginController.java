abe0619de1333676def7f80cf5a85b11561faeb7
juan.otalora <juancarlos.otalora@wnco.com>
2020-02-11 14:42:08 -0500

    Use of map model to give values from controller to the view

--- spring-boot-first-web-application/src/main/java/com/in28minutes/springboot/web/springbootfirstwebapplication/controller/LoginController.java
+++ spring-boot-first-web-application/src/main/java/com/in28minutes/springboot/web/springbootfirstwebapplication/controller/LoginController.java
@@ -1,15 +1,16 @@
 package com.in28minutes.springboot.web.springbootfirstwebapplication.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
 
 	@RequestMapping("/login")
-	public String loginMessage(@RequestParam(name = "name") String name) {
+	public String loginMessage(@RequestParam(name = "name") String name, ModelMap model) {
+		model.put("name", name);
 		System.out.println("name is: " + name);
 		return "login";
 	}

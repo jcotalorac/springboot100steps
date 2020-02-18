864b47f43c8693429ba9ce81784265f37125e59f
juan.otalora <juancarlos.otalora@wnco.com>
2020-02-18 14:32:29 -0500

    Enable name as session attribute in controllers

--- spring-boot-first-web-application/src/main/java/com/in28minutes/springboot/web/springbootfirstwebapplication/controller/TodoController.java
+++ spring-boot-first-web-application/src/main/java/com/in28minutes/springboot/web/springbootfirstwebapplication/controller/TodoController.java
@@ -5,10 +5,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.in28minutes.springboot.web.springbootfirstwebapplication.service.TodoService;
 
 @Controller
+@SessionAttributes("name")
 public class TodoController {
 
 	@Autowired
@@ -16,7 +18,8 @@ public class TodoController {
 	
 	@RequestMapping(value = "/list-todos", method = RequestMethod.GET)
 	public String showTodos(ModelMap model) {
-		model.put("todos", todoService.retrieveTodos("in28minutes"));
+		String name = (String) model.get("name");
+		model.put("todos", todoService.retrieveTodos(name));
 		return "list-todos";
 	}
 }

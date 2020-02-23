9b0c5aa16b67f37f0c48d174d80cbc19fe45c73c
jcotalorac <jcotalorac@localhost>
2020-02-23 18:15:39 -0500

    Binder for controller to manage date format

--- spring-boot-first-web-application/src/main/java/com/in28minutes/springboot/web/springbootfirstwebapplication/controller/TodoController.java
+++ spring-boot-first-web-application/src/main/java/com/in28minutes/springboot/web/springbootfirstwebapplication/controller/TodoController.java
@@ -1,13 +1,17 @@
 package com.in28minutes.springboot.web.springbootfirstwebapplication.controller;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,20 +26,26 @@ public class TodoController {
 
 	@Autowired
 	TodoService todoService;
-	
+
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, false));
+	}
+
 	@RequestMapping(value = "/list-todos", method = RequestMethod.GET)
 	public String showTodos(ModelMap model) {
 		String name = (String) model.get("name");
 		model.put("todos", todoService.retrieveTodos(name));
 		return "list-todos";
 	}
-	
+
 	@RequestMapping(value = "/add-todo", method = RequestMethod.GET)
 	public String showaAddTodoPage(ModelMap model) {
 		model.addAttribute("todo", new Todo(0, (String) model.get("name"), "Default desc", new Date(), false));
 		return "todo";
 	}
-	
+
 	@RequestMapping(value = "/add-todo", method = RequestMethod.POST)
 	public String addTodo(ModelMap model, @Valid Todo todo, BindingResult result) {
 		if (result.hasErrors()) {
@@ -44,21 +54,21 @@ public class TodoController {
 		todoService.addTodo((String) model.get("name"), todo.getDesc(), new Date(), false);
 		return "redirect:/list-todos";
 	}
-	
+
 	@RequestMapping(value = "/delete-todo", method = RequestMethod.GET)
 	public String deleteTodo(@RequestParam("id") int id) {
 		todoService.deleteTodo(id);
 		return "redirect:/list-todos";
 	}
-	
+
 	@RequestMapping(value = "/update-todo", method = RequestMethod.GET)
 	public String showUpdateTodoPage(ModelMap model, @RequestParam("id") int id) {
 		Todo todo = todoService.retrieveTodo(id);
-		//model.addAttribute("todo", todo);
+		// model.addAttribute("todo", todo);
 		model.put("todo", todo);
 		return "todo";
 	}
-	
+
 	@RequestMapping(value = "/update-todo", method = RequestMethod.POST)
 	public String updateTodo(@Valid Todo todo, BindingResult result, ModelMap model) {
 		if (result.hasErrors()) {

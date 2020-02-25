00fb89bb3ae52bd830bd215529b6a518593d04d3
juan.otalora <juancarlos.otalora@wnco.com>
2020-02-25 14:41:51 -0500

    Configuration annotation

--- spring-boot-first-web-application/src/main/java/com/in28minutes/springboot/web/springbootfirstwebapplication/security/SecurityConfiguration.java
+++ spring-boot-first-web-application/src/main/java/com/in28minutes/springboot/web/springbootfirstwebapplication/security/SecurityConfiguration.java
@@ -1,5 +1,8 @@
 package com.in28minutes.springboot.web.springbootfirstwebapplication.security;
 
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SecurityConfiguration {
 
 }

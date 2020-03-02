100621c440c2268e14dc3f2a296ab6f07048270e
juan.otalora <juancarlos.otalora@wnco.com>
2020-03-02 11:24:43 -0500

    Removing name from session attributes, because of getting username from spring security context and put in the model

--- spring-boot-first-web-application/src/main/java/com/in28minutes/springboot/web/springbootfirstwebapplication/controller/LoginController.java
+++ spring-boot-first-web-application/src/main/java/com/in28minutes/springboot/web/springbootfirstwebapplication/controller/LoginController.java
@@ -7,12 +7,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.in28minutes.springboot.web.springbootfirstwebapplication.service.LoginService;
 
 @Controller
-@SessionAttributes("name")
 public class LoginController {
 	
 	@Autowired
--- spring-boot-first-web-application/src/main/java/com/in28minutes/springboot/web/springbootfirstwebapplication/controller/TodoController.java
+++ spring-boot-first-web-application/src/main/java/com/in28minutes/springboot/web/springbootfirstwebapplication/controller/TodoController.java
@@ -15,13 +15,11 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.in28minutes.springboot.web.springbootfirstwebapplication.model.Todo;
 import com.in28minutes.springboot.web.springbootfirstwebapplication.service.TodoService;
 
 @Controller
-@SessionAttributes("name")
 public class TodoController {
 
 	@Autowired

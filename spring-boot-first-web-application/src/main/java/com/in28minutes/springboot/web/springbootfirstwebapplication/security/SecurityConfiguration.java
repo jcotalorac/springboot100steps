e803e8f155fd68365de045b5c4af68db447baf09
juan.otalora <juancarlos.otalora@wnco.com>
2020-03-02 10:59:12 -0500

    Configuration of login page, allowing everybody to /login, and for root (/) and todo pages (/*todo*/**), only to whom have user role

--- spring-boot-first-web-application/src/main/java/com/in28minutes/springboot/web/springbootfirstwebapplication/security/SecurityConfiguration.java
+++ spring-boot-first-web-application/src/main/java/com/in28minutes/springboot/web/springbootfirstwebapplication/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.in28minutes.springboot.web.springbootfirstwebapplication.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
@@ -17,4 +18,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication().withUser("in28minutes").password("{noop}dummy").roles("USER", "ADMIN");
 	}*/
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/login").permitAll()
+		.antMatchers("/", "/*todo*/**").access("hasRole('USER')").and()
+		.formLogin();
+	}
 }

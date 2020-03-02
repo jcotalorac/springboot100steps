9da91bedeb4928fe6f0b8d2e2e1f5c0629a65367
juan.otalora <juancarlos.otalora@wnco.com>
2020-03-02 09:50:25 -0500

    With both snnipets, it's possible to work with inmemory authentication. For password, it's needed the "{noop}" fragment to give the NoOpPassword encoder for plain text passwords

--- spring-boot-first-web-application/src/main/java/com/in28minutes/springboot/web/springbootfirstwebapplication/security/SecurityConfiguration.java
+++ spring-boot-first-web-application/src/main/java/com/in28minutes/springboot/web/springbootfirstwebapplication/security/SecurityConfiguration.java
@@ -1,8 +1,20 @@
 package com.in28minutes.springboot.web.springbootfirstwebapplication.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class SecurityConfiguration {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("in28minutes").password("{noop}dummy").roles("USER", "ADMIN");
+	}
+	
+	/*@Autowired
+	public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("in28minutes").password("{noop}dummy").roles("USER", "ADMIN");
+	}*/
 }

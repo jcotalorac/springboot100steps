561ffc9025f912f9c064f58353f22ca4264dfd4c
jcotalorac <jcotalorac@localhost>
2020-03-23 15:15:13 -0500

    Testing class for MyMath

---
+++ junit-5-steps/test/com/in28minutes/junit/MyMathTest.java
@@ -0,0 +1,14 @@
+package com.in28minutes.junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyMathTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

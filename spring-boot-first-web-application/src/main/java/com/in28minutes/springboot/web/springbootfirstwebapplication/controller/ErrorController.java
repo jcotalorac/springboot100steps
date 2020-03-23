ff9f1679e3605325f7990c836adfc9be13266be0
juan.otalora <juancarlos.otalora@wnco.com>
2020-03-22 21:51:15 -0500

    Error controller annotation

--- spring-boot-first-web-application/src/main/java/com/in28minutes/springboot/web/springbootfirstwebapplication/controller/ErrorController.java
+++ spring-boot-first-web-application/src/main/java/com/in28minutes/springboot/web/springbootfirstwebapplication/controller/ErrorController.java
@@ -1,7 +1,9 @@
 package com.in28minutes.springboot.web.springbootfirstwebapplication.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller("error")
 public class ErrorController {
 
 	public ModelAndView handleException() {

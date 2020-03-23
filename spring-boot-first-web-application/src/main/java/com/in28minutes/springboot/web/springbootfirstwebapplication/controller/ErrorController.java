0dda957f1c0c174828664c592b615f4566f6b159
juan.otalora <juancarlos.otalora@wnco.com>
2020-03-22 21:52:26 -0500

    Exception handler method annotation, with exception type

--- spring-boot-first-web-application/src/main/java/com/in28minutes/springboot/web/springbootfirstwebapplication/controller/ErrorController.java
+++ spring-boot-first-web-application/src/main/java/com/in28minutes/springboot/web/springbootfirstwebapplication/controller/ErrorController.java
@@ -1,11 +1,13 @@
 package com.in28minutes.springboot.web.springbootfirstwebapplication.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller("error")
 public class ErrorController {
 
+	@ExceptionHandler(Exception.class)
 	public ModelAndView handleException() {
 		return null;
 	}

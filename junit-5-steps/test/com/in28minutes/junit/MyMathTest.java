f506460b8d9df6a9a094b0b5b965ec680f70fb0d
jcotalorac <jcotalorac@localhost>
2020-03-23 15:29:52 -0500

    Unexpected failure test

--- junit-5-steps/test/com/in28minutes/junit/MyMathTest.java
+++ junit-5-steps/test/com/in28minutes/junit/MyMathTest.java
@@ -1,11 +1,17 @@
 package com.in28minutes.junit;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class MyMathTest {
 
 	@Test
-	public void test() {
-	}
+	public void sumWith3Numbers() {
+		MyMath myMath = new MyMath();
 
+		int result = myMath.sum(new int[] { 1, 2, 3 });
+
+		assertEquals(5, result);
+	}
 }

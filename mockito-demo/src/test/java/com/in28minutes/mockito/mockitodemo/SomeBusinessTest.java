27c8ae0e19c923dcb9500defcfed776716310437
jcotalorac <jcotalorac@localhost>
2020-03-23 19:31:21 -0500

    Junit test case

---
+++ mockito-demo/src/test/java/com/in28minutes/mockito/mockitodemo/SomeBusinessTest.java
@@ -0,0 +1,14 @@
+package com.in28minutes.mockito.mockitodemo;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class SomeBusinessTest {
+
+	@Test
+	public void testFindGreatestFromAllData() {
+		fail("Not yet implemented");
+	}
+
+}

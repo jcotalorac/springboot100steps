00a1997979f21edfeddf245fa0da7fcbeeabfb62
jcotalorac <jcotalorac@localhost>
2020-03-23 22:28:59 -0500

    New junit test case

---
+++ mockito-demo/src/test/java/com/in28minutes/mockito/mockitodemo/ListTest.java
@@ -0,0 +1,14 @@
+package com.in28minutes.mockito.mockitodemo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ListTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

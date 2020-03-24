365e49e6704fbf630aada1634c365b484ffe14ea
jcotalorac <jcotalorac@localhost>
2020-03-24 18:41:33 -0500

    Running springboot application

--- first-springboot-project/src/main/java/com/in28minutes/springboot/Application.java
+++ first-springboot-project/src/main/java/com/in28minutes/springboot/Application.java
@@ -1,10 +1,14 @@
 package com.in28minutes.springboot;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ApplicationContext ctx = SpringApplication.run(Application.class, args);
 	}
 
 }

92396993471f62367cf7ed1991170eea54bdb394
jcotalorac <jcotalorac@localhost>
2020-03-23 19:34:05 -0500

    Null pointer exception because of a non data retrieving

--- mockito-demo/src/test/java/com/in28minutes/mockito/mockitodemo/SomeBusinessTest.java
+++ mockito-demo/src/test/java/com/in28minutes/mockito/mockitodemo/SomeBusinessTest.java
@@ -1,14 +1,13 @@
 package com.in28minutes.mockito.mockitodemo;
 
-import static org.junit.Assert.fail;
-
 import org.junit.Test;
 
 public class SomeBusinessTest {
 
 	@Test
 	public void testFindGreatestFromAllData() {
-		fail("Not yet implemented");
+		SomeBusinessImpl businessImpl = new SomeBusinessImpl();
+		businessImpl.findGreatestFromAllData();
 	}
 
 }

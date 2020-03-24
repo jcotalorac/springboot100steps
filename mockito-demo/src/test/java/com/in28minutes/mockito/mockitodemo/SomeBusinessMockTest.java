4deb4ad9e9d5d4b01862ac5784f3af3e4ec3923d
jcotalorac <jcotalorac@localhost>
2020-03-23 20:27:48 -0500

    Test using mock

--- mockito-demo/src/test/java/com/in28minutes/mockito/mockitodemo/SomeBusinessMockTest.java
+++ mockito-demo/src/test/java/com/in28minutes/mockito/mockitodemo/SomeBusinessMockTest.java
@@ -1,6 +1,8 @@
 package com.in28minutes.mockito.mockitodemo;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.Test;
 
@@ -8,7 +10,13 @@ public class SomeBusinessMockTest {
 
 	@Test
 	public void testFindGreatestFromAllData() {
-		fail("Not yet implemented");
+
+		DataService dataServiceMock = mock(DataService.class);
+		when(dataServiceMock.retrieveAllData()).thenReturn(new int[] { 24, 15, 3 });
+
+		SomeBusinessImpl businessImpl = new SomeBusinessImpl(dataServiceMock);
+		int result = businessImpl.findGreatestFromAllData();
+		assertEquals(24, result);
 	}
 
 }

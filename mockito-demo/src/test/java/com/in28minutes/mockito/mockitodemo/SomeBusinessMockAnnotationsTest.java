7f047361ab20c140e0a2808e967a280684435ade
jcotalorac <jcotalorac@localhost>
2020-03-23 22:05:44 -0500

    Junit test case using mockito annotations

---
+++ mockito-demo/src/test/java/com/in28minutes/mockito/mockitodemo/SomeBusinessMockAnnotationsTest.java
@@ -0,0 +1,14 @@
+package com.in28minutes.mockito.mockitodemo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SomeBusinessMockAnnotationsTest {
+
+	@Test
+	public void testFindGreatestFromAllData() {
+		fail("Not yet implemented");
+	}
+
+}

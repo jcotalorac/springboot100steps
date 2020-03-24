8e352166d97b058e5415011f5fdcef4cfba404dc
jcotalorac <jcotalorac@localhost>
2020-03-23 22:13:34 -0500

    Using mock and injectmocks annotations

--- mockito-demo/src/test/java/com/in28minutes/mockito/mockitodemo/SomeBusinessMockAnnotationsTest.java
+++ mockito-demo/src/test/java/com/in28minutes/mockito/mockitodemo/SomeBusinessMockAnnotationsTest.java
@@ -1,14 +1,26 @@
 package com.in28minutes.mockito.mockitodemo;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 
 public class SomeBusinessMockAnnotationsTest {
+	
+	@Mock
+	DataService dataServiceMock;
+	
+	@InjectMocks
+	private SomeBusinessImpl businessImpl;
 
 	@Test
 	public void testFindGreatestFromAllData() {
-		fail("Not yet implemented");
+		when(dataServiceMock.retrieveAllData()).thenReturn(new int[] { 24, 15, 3 });
+
+		int result = businessImpl.findGreatestFromAllData();
+		assertEquals(24, result);
 	}
 
 }

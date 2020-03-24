020d5f1cdb3d3674fff7479c7a08187f0d4eb158
jcotalorac <jcotalorac@localhost>
2020-03-24 18:51:28 -0500

    Rest controller and request mapping to method message

--- first-springboot-project/src/main/java/com/in28minutes/springboot/WelcomeController.java
+++ first-springboot-project/src/main/java/com/in28minutes/springboot/WelcomeController.java
@@ -1,7 +1,12 @@
 package com.in28minutes.springboot;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class WelcomeController {
 
+	@RequestMapping("/welcome")
 	public String welcome() {
 		return "This is great! I see something on screen";
 	}

773b85590f88d07136bceb277056142815740001
jcotalorac <jcotalorac@localhost>
2020-03-23 20:04:35 -0500

    New junit test case for somebusiness class

---
+++ mockito-demo/src/test/java/com/in28minutes/mockito/mockitodemo/SomeBusinessMockTest.java
@@ -0,0 +1,14 @@
+package com.in28minutes.mockito.mockitodemo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SomeBusinessMockTest {
+
+	@Test
+	public void testFindGreatestFromAllData() {
+		fail("Not yet implemented");
+	}
+
+}

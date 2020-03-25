af1f245a65afc8f2d4084fe94ca5a64838de0214
jcotalorac <jcotalorac@localhost>
2020-03-25 12:46:48 -0500

    Adding question by mean of post request

--- first-springboot-project/src/main/java/com/in28minutes/springboot/controller/SurveyController.java
+++ first-springboot-project/src/main/java/com/in28minutes/springboot/controller/SurveyController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.in28minutes.springboot.model.Question;
@@ -19,6 +21,11 @@ public class SurveyController {
 	@GetMapping("/surveys/{surveyId}/questions")
 	public List<Question> retrieveQuestionsForSurvey(@PathVariable String surveyId) {
 		return surveyService.retrieveQuestions(surveyId);
+	}
+	
+	@PostMapping("/surveys/{surveyId}/questions")
+	public Question addQuestionToSurvey(@PathVariable String surveyId, @RequestBody Question newQuestion) {
+		return surveyService.addQuestion(surveyId, newQuestion);
 	}
 	
 	@GetMapping("/surveys/{surveyId}/questions/{questionId}")

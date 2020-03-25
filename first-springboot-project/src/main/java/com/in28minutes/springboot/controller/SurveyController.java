d62ee382f43cfbc79bfc01e91d52da7990a32c6c
jcotalorac <jcotalorac@localhost>
2020-03-24 22:13:21 -0500

    Autowiring service dependency

--- first-springboot-project/src/main/java/com/in28minutes/springboot/controller/SurveyController.java
+++ first-springboot-project/src/main/java/com/in28minutes/springboot/controller/SurveyController.java
@@ -1,8 +1,13 @@
 package com.in28minutes.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.in28minutes.springboot.service.SurveyService;
 
 @RestController
 public class SurveyController {
 
+	@Autowired
+	private SurveyService surveyService;
 }

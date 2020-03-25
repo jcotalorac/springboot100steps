5de5ee138a7bff6bcda4f161241c507b77814798
jcotalorac <jcotalorac@localhost>
2020-03-24 21:47:28 -0500

    Model classes

---
+++ first-springboot-project/src/main/java/com/in28minutes/springboot/model/Question.java
@@ -0,0 +1,101 @@
+package com.in28minutes.springboot.model;
+
+import java.util.List;
+
+public class Question {
+
+	private String id;
+	private String description;
+	private String correctAnswer;
+	private List<String> options;
+
+	public Question(String id, String description, String correctAnswer, List<String> options) {
+		super();
+		this.id = id;
+		this.description = description;
+		this.correctAnswer = correctAnswer;
+		this.options = options;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getCorrectAnswer() {
+		return correctAnswer;
+	}
+
+	public void setCorrectAnswer(String correctAnswer) {
+		this.correctAnswer = correctAnswer;
+	}
+
+	public List<String> getOptions() {
+		return options;
+	}
+
+	public void setOptions(List<String> options) {
+		this.options = options;
+	}
+
+	@Override
+	public String toString() {
+		return "Question [id=" + id + ", description=" + description + ", correctAnswer=" + correctAnswer + ", options="
+				+ options + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((correctAnswer == null) ? 0 : correctAnswer.hashCode());
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((options == null) ? 0 : options.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Question other = (Question) obj;
+		if (correctAnswer == null) {
+			if (other.correctAnswer != null)
+				return false;
+		} else if (!correctAnswer.equals(other.correctAnswer))
+			return false;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (options == null) {
+			if (other.options != null)
+				return false;
+		} else if (!options.equals(other.options))
+			return false;
+		return true;
+	}
+
+}

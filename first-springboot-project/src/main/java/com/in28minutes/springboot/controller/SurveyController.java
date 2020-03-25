45897279172e2ee7b876fe26019312dffb983ddf
jcotalorac <jcotalorac@localhost>
2020-03-25 13:04:14 -0500

    Returning location and created status in response headers

--- first-springboot-project/src/main/java/com/in28minutes/springboot/controller/SurveyController.java
+++ first-springboot-project/src/main/java/com/in28minutes/springboot/controller/SurveyController.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,20 +20,22 @@ public class SurveyController {
 
 	@Autowired
 	private SurveyService surveyService;
-	
+
 	@GetMapping("/surveys/{surveyId}/questions")
 	public List<Question> retrieveQuestionsForSurvey(@PathVariable String surveyId) {
 		return surveyService.retrieveQuestions(surveyId);
 	}
-	
+
 	@PostMapping("/surveys/{surveyId}/questions")
-	public Question addQuestionToSurvey(@PathVariable String surveyId, @RequestBody Question newQuestion) {
+	public ResponseEntity<Void> addQuestionToSurvey(@PathVariable String surveyId, @RequestBody Question newQuestion) {
 		Question question = surveyService.addQuestion(surveyId, newQuestion);
+
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(question.getId())
+				.toUri();
 		
-		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("{id}").buildAndExpand(question.getId()).toUri();
-		return question;
+		return ResponseEntity.created(location).build();
 	}
-	
+
 	@GetMapping("/surveys/{surveyId}/questions/{questionId}")
 	public Question retrieveDetailsForQuestion(@PathVariable String surveyId, @PathVariable String questionId) {
 		return surveyService.retrieveQuestion(surveyId, questionId);

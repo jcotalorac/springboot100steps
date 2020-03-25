c618973ec12949220fbe5d83dde8d629071fba48
jcotalorac <jcotalorac@localhost>
2020-03-24 22:15:26 -0500

    Signature for exposing method

--- first-springboot-project/src/main/java/com/in28minutes/springboot/controller/SurveyController.java
+++ first-springboot-project/src/main/java/com/in28minutes/springboot/controller/SurveyController.java
@@ -1,8 +1,11 @@
 package com.in28minutes.springboot.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.in28minutes.springboot.model.Question;
 import com.in28minutes.springboot.service.SurveyService;
 
 @RestController
@@ -10,4 +13,8 @@ public class SurveyController {
 
 	@Autowired
 	private SurveyService surveyService;
+	
+	public List<Question> retrieveQuestionsForSurvey(String surveyId) {
+		return null;
+	}
 }

d920ee9b7e3d0ce004c2703a0bdbf33edfe051e3
jcotalorac <jcotalorac@localhost>
2020-03-24 20:00:11 -0500

    Moving service class to base package

--- first-springboot-project/src/main/java/com/in28minutes/springboot/WelcomeController.java
+++ first-springboot-project/src/main/java/com/in28minutes/springboot/WelcomeController.java
@@ -1,11 +1,8 @@
 package com.in28minutes.springboot;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.in28minutes.service.WelcomeService;
 
 @RestController
 public class WelcomeController {

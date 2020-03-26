c0818c6d916c5fd0b45524fd8915b07d0ff1e2fa
jcotalorac <jcotalorac@localhost>
2020-03-26 17:33:31 -0500

    Autogenerated value

--- first-springboot-project/src/main/java/com/in28minutes/springboot/jpa/User.java
+++ first-springboot-project/src/main/java/com/in28minutes/springboot/jpa/User.java
@@ -1,12 +1,15 @@
 package com.in28minutes.springboot.jpa;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class User {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private String name;
 	private String role;

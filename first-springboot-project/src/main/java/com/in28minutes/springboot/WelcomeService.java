2f2f836a6f51ad5804a1b9fe5d28464bfbdf3742
jcotalorac <jcotalorac@localhost>
2020-03-25 22:31:01 -0500

    Get value from property file

--- first-springboot-project/src/main/java/com/in28minutes/springboot/WelcomeService.java
+++ first-springboot-project/src/main/java/com/in28minutes/springboot/WelcomeService.java
@@ -1,10 +1,15 @@
 package com.in28minutes.springboot;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class WelcomeService {
+	
+	@Value("${welcome.message}")
+	private String welcomeMessage;
+	
 	public String retrieveWelcomeMessage() {
-		return "Good Morning updated! 123";
+		return welcomeMessage;
 	}
 }

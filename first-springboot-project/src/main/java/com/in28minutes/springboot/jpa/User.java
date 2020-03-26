4bfe1d3713e0ba304b2c8d88d582378af9dc33b1
jcotalorac <jcotalorac@localhost>
2020-03-26 17:31:26 -0500

    Marking entity class and id field

--- first-springboot-project/src/main/java/com/in28minutes/springboot/jpa/User.java
+++ first-springboot-project/src/main/java/com/in28minutes/springboot/jpa/User.java
@@ -1,7 +1,12 @@
 package com.in28minutes.springboot.jpa;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class User {
 
+	@Id
 	private Long id;
 	private String name;
 	private String role;

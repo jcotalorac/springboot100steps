f9852bcc360632fc64ce0766d5e32fb0de6c0a71
jcotalorac <jcotalorac@localhost>
2020-03-26 11:09:16 -0500

    Method to return properties from type safe configuration

--- first-springboot-project/src/main/java/com/in28minutes/springboot/WelcomeController.java
+++ first-springboot-project/src/main/java/com/in28minutes/springboot/WelcomeController.java
@@ -1,5 +1,7 @@
 package com.in28minutes.springboot;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,5 +15,9 @@ public class WelcomeController {
 	@RequestMapping("/welcome")
 	public String welcome() {
 		return service.retrieveWelcomeMessage();
+	}
+	
+	public Map dynamicConfiguration() {
+		return null;
 	}
 }

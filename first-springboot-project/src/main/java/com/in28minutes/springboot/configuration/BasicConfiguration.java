53a2a3eb032e7c0fdf8db81aef8049949d0186e9
jcotalorac <jcotalorac@localhost>
2020-03-26 11:00:42 -0500

    Basic configuration class for type safe configuration

---
+++ first-springboot-project/src/main/java/com/in28minutes/springboot/configuration/BasicConfiguration.java
@@ -0,0 +1,33 @@
+package com.in28minutes.springboot.configuration;
+
+public class BasicConfiguration {
+
+	private boolean value;
+	private String message;
+	private int number;
+
+	public boolean isValue() {
+		return value;
+	}
+
+	public void setValue(boolean value) {
+		this.value = value;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+}

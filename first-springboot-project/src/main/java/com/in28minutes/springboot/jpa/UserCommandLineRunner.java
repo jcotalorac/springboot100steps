eb2623a628154b2dca43b287259ed2ec582f4b4e
jcotalorac <jcotalorac@localhost>
2020-03-26 18:03:24 -0500

    Using user repository in the starting of application

--- first-springboot-project/src/main/java/com/in28minutes/springboot/jpa/UserCommandLineRunner.java
+++ first-springboot-project/src/main/java/com/in28minutes/springboot/jpa/UserCommandLineRunner.java
@@ -1,14 +1,17 @@
 package com.in28minutes.springboot.jpa;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserCommandLineRunner implements CommandLineRunner {
 
+	@Autowired
+	private UserRepository userRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("UserCommandLineRunner");
 	}
 
 }

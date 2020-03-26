18db1bace62966ad4cb2462bce207ac2975fa1c4
jcotalorac <jcotalorac@localhost>
2020-03-26 17:58:39 -0500

    Marking component and launcher for command line

--- first-springboot-project/src/main/java/com/in28minutes/springboot/jpa/UserCommandLineRunner.java
+++ first-springboot-project/src/main/java/com/in28minutes/springboot/jpa/UserCommandLineRunner.java
@@ -1,5 +1,14 @@
 package com.in28minutes.springboot.jpa;
 
-public class UserCommandLineRunner {
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserCommandLineRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("UserCommandLineRunner");
+	}
 
 }

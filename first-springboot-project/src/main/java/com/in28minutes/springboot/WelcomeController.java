08d271e7c663f8d37eddd5573629caae0b993e80
jcotalorac <jcotalorac@localhost>
2020-03-26 11:13:19 -0500

    Using basic configuration component in controller

--- first-springboot-project/src/main/java/com/in28minutes/springboot/WelcomeController.java
+++ first-springboot-project/src/main/java/com/in28minutes/springboot/WelcomeController.java
@@ -6,11 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.in28minutes.springboot.configuration.BasicConfiguration;
+
 @RestController
 public class WelcomeController {
 	
 	@Autowired
 	private WelcomeService service;
+	
+	@Autowired
+	private BasicConfiguration basicConfiguration;
 
 	@RequestMapping("/welcome")
 	public String welcome() {

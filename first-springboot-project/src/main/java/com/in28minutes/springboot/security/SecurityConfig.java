eb278737a503a4def487ce775b5d5d8d66497951
jcotalorac <jcotalorac@localhost>
2020-03-27 14:03:08 -0500

    Configuring in memory authentication and authorization

--- first-springboot-project/src/main/java/com/in28minutes/springboot/security/SecurityConfig.java
+++ first-springboot-project/src/main/java/com/in28minutes/springboot/security/SecurityConfig.java
@@ -1,8 +1,14 @@
 package com.in28minutes.springboot.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class SecurityConfig {
-
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("user1").password("secret1").roles("USER").and()
+		.withUser("admin1").password("secret1").roles("USER", "ADMIN");
+	}
 }

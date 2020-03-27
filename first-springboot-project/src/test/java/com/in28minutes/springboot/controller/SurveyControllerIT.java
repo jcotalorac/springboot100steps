7024df77f7da84d2b7601173d05f9bbd772f5700
jcotalorac <jcotalorac@localhost>
2020-03-26 22:14:32 -0500

    Using exchange to get the response entity

--- first-springboot-project/src/test/java/com/in28minutes/springboot/controller/SurveyControllerIT.java
+++ first-springboot-project/src/test/java/com/in28minutes/springboot/controller/SurveyControllerIT.java
@@ -5,6 +5,8 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.in28minutes.springboot.Application;
@@ -20,7 +22,8 @@ public class SurveyControllerIT {
 	public void test() {
 		String url = "http://localhost:" + port + "/surveys/Survey1/questions/Question1";
 		TestRestTemplate restTemplate = new TestRestTemplate();
-		String output = restTemplate.getForObject(url, String.class);
-		System.out.println("Response: " + output);
+		
+		ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.GET, null, String.class);
+		System.out.println("Response: " + responseEntity.getBody());
 	}
 }

a39e45338d7e022ef01b91d2a401e9accd8e6c6e
jcotalorac <jcotalorac@localhost>
2020-03-27 18:58:19 -0500

    Implementing save user

--- jpa-in-10-steps/src/main/java/com/in28minutes/learning/jpa/jpain10steps/service/UserDAOService.java
+++ jpa-in-10-steps/src/main/java/com/in28minutes/learning/jpa/jpain10steps/service/UserDAOService.java
@@ -1,5 +1,15 @@
 package com.in28minutes.learning.jpa.jpain10steps.service;
 
+import javax.persistence.EntityManager;
+
+import com.in28minutes.learning.jpa.jpain10steps.entity.User;
+
 public class UserDAOService {
 
+	private EntityManager entityManager;
+
+	public long insert(User user) {
+		entityManager.persist(user);
+		return user.getId();
+	}
 }

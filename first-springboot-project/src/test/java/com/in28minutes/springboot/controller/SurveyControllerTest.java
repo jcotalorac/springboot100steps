d6c191745f587a117cfd0c02c75018fb8ab07945
jcotalorac <jcotalorac@localhost>
2020-03-27 10:30:52 -0500

    Mocking dependency with mock data by mean of a precondition

--- first-springboot-project/src/test/java/com/in28minutes/springboot/controller/SurveyControllerTest.java
+++ first-springboot-project/src/test/java/com/in28minutes/springboot/controller/SurveyControllerTest.java
@@ -1,16 +1,33 @@
 package com.in28minutes.springboot.controller;
 
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.in28minutes.springboot.model.Question;
+import com.in28minutes.springboot.service.SurveyService;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(SurveyController.class)
 public class SurveyControllerTest {
 
+	@MockBean
+	private SurveyService surveyService;
+	
 	@Test
-	public void testMethod() {
+	public void testRetrieveDetailsForQuestion() {
+		
+		Question mockQuestion = new Question("Question1", "Largest Country in the World", "Russia",
+				Arrays.asList("India", "Russia", "United States", "China"));
+		
+		when(surveyService.retrieveQuestion(anyString(), anyString())).thenReturn(mockQuestion);
 	}
 
 }

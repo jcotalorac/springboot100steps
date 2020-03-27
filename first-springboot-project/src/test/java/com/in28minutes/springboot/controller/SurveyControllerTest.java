a045d436279a45e498c949774215f6469d7b23c1
jcotalorac <jcotalorac@localhost>
2020-03-27 12:52:06 -0500

    Unit test for post rest service, verifying location and status

--- first-springboot-project/src/test/java/com/in28minutes/springboot/controller/SurveyControllerTest.java
+++ first-springboot-project/src/test/java/com/in28minutes/springboot/controller/SurveyControllerTest.java
@@ -1,5 +1,7 @@
 package com.in28minutes.springboot.controller;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.when;
 
@@ -11,7 +13,10 @@ import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -45,6 +50,25 @@ public class SurveyControllerTest {
 		String expected = "{\"id\":\"Question1\",\"description\":\"Largest Country in the World\",\"correctAnswer\":\"Russia\",\"options\":[\"India\",\"Russia\",\"United States\",\"China\"]}";
 
 		JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), true);
+	}
+
+	@Test
+	public void testCreateSurveyQuestion() throws Exception {
+
+		Question mockQuestion = new Question("1", "Smallest Number", "1", Arrays.asList("1", "2", "3", "4"));
+
+		String questionJsonRequest = "{\"description\":\"Smallest Number\",\"correctAnswer\":\"1\",\"options\":[\"1\",\"2\",\"3\",\"4\"]}";
+
+		when(surveyService.addQuestion(anyString(), any(Question.class))).thenReturn(mockQuestion);
+
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/surveys/Survey1/questions")
+				.accept(MediaType.APPLICATION_JSON).content(questionJsonRequest).contentType(MediaType.APPLICATION_JSON);
+		
+		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+		MockHttpServletResponse response = result.getResponse();
+		assertEquals(HttpStatus.CREATED.value(), response.getStatus());
+		
+		assertEquals("http://localhost/surveys/Survey1/questions/1", response.getHeader(HttpHeaders.LOCATION));
 	}
 
 }

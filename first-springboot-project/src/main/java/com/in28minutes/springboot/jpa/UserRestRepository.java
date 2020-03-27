e89a4f104a021d14c49f0893e9480b20366f6980
jcotalorac <jcotalorac@localhost>
2020-03-26 19:26:15 -0500

    Exposing repository to rest service

--- first-springboot-project/src/main/java/com/in28minutes/springboot/jpa/UserRestRepository.java
+++ first-springboot-project/src/main/java/com/in28minutes/springboot/jpa/UserRestRepository.java
@@ -3,7 +3,9 @@ package com.in28minutes.springboot.jpa;
 import java.util.List;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "users", collectionResourceRel = "users")
 public interface UserRestRepository extends PagingAndSortingRepository<User, Long> {
 
 	List<User> findByRole(String role);

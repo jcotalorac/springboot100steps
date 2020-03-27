130bc9b99d30dbb31d65b747d34992509afbb485
jcotalorac <jcotalorac@localhost>
2020-03-26 23:00:58 -0500

    Json asser failed, because of a different value in attribute

--- first-springboot-project/src/test/java/com/in28minutes/springboot/controller/SurveyControllerIT.java
+++ first-springboot-project/src/test/java/com/in28minutes/springboot/controller/SurveyControllerIT.java
@@ -29,7 +29,7 @@ public class SurveyControllerIT {
 	@Test
 	public void testJsonAssert() {
 
-		String actual = "{id:1,name:Ranga}";
+		String actual = "{id:2,name:Ranga}";
 		JSONAssert.assertEquals("{id:1}", actual, false);
 
 	}

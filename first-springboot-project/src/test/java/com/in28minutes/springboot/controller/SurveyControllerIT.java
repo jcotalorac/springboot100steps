d4cf47b11a90a4481fe173d2b6779c4120700627
jcotalorac <jcotalorac@localhost>
2020-03-26 22:06:12 -0500

    Creating a rest template test and invoking service

--- first-springboot-project/src/test/java/com/in28minutes/springboot/controller/SurveyControllerIT.java
+++ first-springboot-project/src/test/java/com/in28minutes/springboot/controller/SurveyControllerIT.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.in28minutes.springboot.Application;
@@ -14,9 +15,12 @@ public class SurveyControllerIT {
 
 	@LocalServerPort
 	private int port;
-	
+
 	@Test
 	public void test() {
-		System.out.println("PORT: " + port);
+		String url = "http://localhost:" + port + "/surveys/Survey1/questions/Question1";
+		TestRestTemplate restTemplate = new TestRestTemplate();
+		String output = restTemplate.getForObject(url, String.class);
+		System.out.println("Response: " + output);
 	}
 }

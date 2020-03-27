c8000d93f7ccf7ee98cfae74ddf5ffdacf51e872
jcotalorac <jcotalorac@localhost>
2020-03-26 21:15:48 -0500

    Launching integration starting spring boot to execute test showing random port

--- first-springboot-project/src/test/java/com/in28minutes/springboot/controller/SurveyControllerIT.java
+++ first-springboot-project/src/test/java/com/in28minutes/springboot/controller/SurveyControllerIT.java
@@ -2,6 +2,7 @@ package com.in28minutes.springboot.controller;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,7 +12,11 @@ import com.in28minutes.springboot.Application;
 @SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SurveyControllerIT {
 
+	@LocalServerPort
+	private int port;
+	
 	@Test
 	public void test() {
+		System.out.println("PORT: " + port);
 	}
 }

710843ab47632cdf0a456d99950625c94be6a4df
jcotalorac <jcotalorac@localhost>
2020-03-27 00:19:54 -0500

    Integration test for post method

--- first-springboot-project/src/test/java/com/in28minutes/springboot/controller/SurveyControllerIT.java
+++ first-springboot-project/src/test/java/com/in28minutes/springboot/controller/SurveyControllerIT.java
@@ -18,6 +18,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.in28minutes.springboot.Application;
+import com.in28minutes.springboot.model.Question;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -34,13 +35,34 @@ public class SurveyControllerIT {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 
-		HttpEntity<String> entityRequest = new HttpEntity<>(null, headers);
+		HttpEntity<String> requestEntity = new HttpEntity<>(null, headers);
 
-		ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.GET, entityRequest, String.class);
+		ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.GET, requestEntity, String.class);
 		System.out.println("Response: " + responseEntity.getBody());
 
 		String expected = "{\"id\":\"Question1\",\"description\":\"Largest Country in the World\",\"correctAnswer\":\"Russia\",\"options\":[\"India\",\"Russia\",\"United States\",\"China\"]}";
 		JSONAssert.assertEquals(expected, responseEntity.getBody(), true);
 		assertTrue(responseEntity.getBody().contains("\"id\":\"Question1\""));
+	}
+
+	@Test
+	public void testAddQuestion() {
+		String url = "http://localhost:" + port + "/surveys/Survey1/questions";
+		TestRestTemplate restTemplate = new TestRestTemplate();
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+
+		Question question = new Question("Question1", "Largest Country in the World", "Russia",
+				Arrays.asList("India", "Russia", "United States", "China"));
+		HttpEntity<Question> requestEntity = new HttpEntity<Question>(question, headers);
+
+		ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.POST, requestEntity,
+				String.class);
+		
+		String actual = responseEntity.getHeaders().get(HttpHeaders.LOCATION).get(0);
+		assertTrue(actual.contains("/surveys/Survey1/questions"));
+		System.out.println("Actual: " + actual);
+
 	}
 }

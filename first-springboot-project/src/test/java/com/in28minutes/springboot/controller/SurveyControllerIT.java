62b5b80a8260b032d3d67af1bceb1d2473c2fb46
jcotalorac <jcotalorac@localhost>
2020-03-26 21:00:31 -0500

    Basic test for integration

--- first-springboot-project/src/test/java/com/in28minutes/springboot/controller/SurveyControllerIT.java
+++ first-springboot-project/src/test/java/com/in28minutes/springboot/controller/SurveyControllerIT.java
@@ -1,29 +1,10 @@
 package com.in28minutes.springboot.controller;
 
-import static org.junit.Assert.fail;
-
 import org.junit.Test;
 
 public class SurveyControllerIT {
 
 	@Test
-	public void testRetrieveQuestionsForSurvey() {
-		fail("Not yet implemented");
+	public void test() {
 	}
-
-	@Test
-	public void testAddQuestionToSurvey() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testRetrieveDetailsForQuestion() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testRetrieveAllSurveys() {
-		fail("Not yet implemented");
-	}
-
 }

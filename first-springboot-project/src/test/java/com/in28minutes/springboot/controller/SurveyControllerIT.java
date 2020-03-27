a8a539a948d9a1e95f0eedc4876363f5e31d4004
jcotalorac <jcotalorac@localhost>
2020-03-26 22:47:39 -0500

    Json assert test realizing on expected attributes

--- first-springboot-project/src/test/java/com/in28minutes/springboot/controller/SurveyControllerIT.java
+++ first-springboot-project/src/test/java/com/in28minutes/springboot/controller/SurveyControllerIT.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -18,23 +19,31 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import com.in28minutes.springboot.Application;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SurveyControllerIT {
 
-	@LocalServerPort
+	//@LocalServerPort
 	private int port;
+
+	@Test
+	public void testJsonAssert() {
+
+		String actual = "{id:1,name:Ranga}";
+		JSONAssert.assertEquals("{id:1}", actual, false);
+
+	}
 
 	@Test
 	public void test() {
 		String url = "http://localhost:" + port + "/surveys/Survey1/questions/Question1";
 		TestRestTemplate restTemplate = new TestRestTemplate();
-		
+
 		HttpHeaders headers = new HttpHeaders();
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-		
+
 		HttpEntity<String> entityRequest = new HttpEntity<>(null, headers);
-		
+
 		ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.GET, entityRequest, String.class);
 		System.out.println("Response: " + responseEntity.getBody());
 		assertTrue(responseEntity.getBody().contains("\"id\":\"Question1\""));

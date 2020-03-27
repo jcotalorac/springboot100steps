bcb5ca9eaff8782aa0ba557c4ea03a1589c38e5f
jcotalorac <jcotalorac@localhost>
2020-03-26 21:06:46 -0500

    Classes for spring boot launching in test

--- first-springboot-project/src/test/java/com/in28minutes/springboot/controller/SurveyControllerIT.java
+++ first-springboot-project/src/test/java/com/in28minutes/springboot/controller/SurveyControllerIT.java
@@ -2,9 +2,13 @@ package com.in28minutes.springboot.controller;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.in28minutes.springboot.Application;
+
 @RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
 public class SurveyControllerIT {
 
 	@Test

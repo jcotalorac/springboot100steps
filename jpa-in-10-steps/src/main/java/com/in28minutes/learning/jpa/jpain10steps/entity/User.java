fadbaa4a445ceb0face38cc8d09176e2de10c9ae
jcotalorac <jcotalorac@localhost>
2020-03-27 18:50:56 -0500

    Marking as entity and id as generated

--- jpa-in-10-steps/src/main/java/com/in28minutes/learning/jpa/jpain10steps/entity/User.java
+++ jpa-in-10-steps/src/main/java/com/in28minutes/learning/jpa/jpain10steps/entity/User.java
@@ -1,14 +1,21 @@
 package com.in28minutes.learning.jpa.jpain10steps.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User {
 
+	@Id
+	@GeneratedValue
 	private Long id;
 	private String name;
 	private String role;
 
 	protected User() {
 	}
-	
+
 	public User(String name, String role) {
 		super();
 		this.name = name;

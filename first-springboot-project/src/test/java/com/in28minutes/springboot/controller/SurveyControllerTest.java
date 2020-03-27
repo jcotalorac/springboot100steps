a340c2508852bd97454f1e2c1338a9b572547487
jcotalorac <jcotalorac@localhost>
2020-03-27 09:42:02 -0500

    Configuring class to launch

--- first-springboot-project/src/test/java/com/in28minutes/springboot/controller/SurveyControllerTest.java
+++ first-springboot-project/src/test/java/com/in28minutes/springboot/controller/SurveyControllerTest.java
@@ -2,9 +2,11 @@ package com.in28minutes.springboot.controller;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@WebMvcTest(SurveyController.class)
 public class SurveyControllerTest {
 
 	@Test

a7bad54d8833037ee6484a3918b563eff65daa32
jcotalorac <jcotalorac@localhost>
2020-03-27 23:57:27 -0500

    Sql resource to load database on launch application

---
+++ spring-boot-first-web-application/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into TODO
+values(10001, sysdate(), 'Learn Spring Boot', false, 'in28minutes');
+insert into TODO
+values(10002, sysdate(), 'Learn Angular JS', false, 'in28minutes');
+insert into TODO
+values(10003, sysdate(), 'Learn to Dance', false, 'in28minutes');

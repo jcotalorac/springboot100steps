7c35d7066ee7ee75df7ba0900a7dff21598a3556
jcotalorac <jcotalorac@localhost>
2020-03-27 19:01:42 -0500

    Marking class as a repository to indicate interaction with database

--- jpa-in-10-steps/src/main/java/com/in28minutes/learning/jpa/jpain10steps/service/UserDAOService.java
+++ jpa-in-10-steps/src/main/java/com/in28minutes/learning/jpa/jpain10steps/service/UserDAOService.java
@@ -2,8 +2,11 @@ package com.in28minutes.learning.jpa.jpain10steps.service;
 
 import javax.persistence.EntityManager;
 
+import org.springframework.stereotype.Repository;
+
 import com.in28minutes.learning.jpa.jpain10steps.entity.User;
 
+@Repository
 public class UserDAOService {
 
 	private EntityManager entityManager;
